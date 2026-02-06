5cfece83a18b4e6043bef0d2fe4c3de0bf34ab4e
zhanghao <zhanghao@jiigan.com>
2026-02-06 12:46:23 +0800

    [feat]: nvim auto install LSP clangd

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,12 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
+local servers = { "clangd" }
+
+mason_lspconfig.setup({
+    ensure_installed = servers,
+    automatic_installation = true,
+})
+
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 

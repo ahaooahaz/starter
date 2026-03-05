988d3e49d63121d66c02fb063a16202e546e46ef
ahaooahaz <ahaooahaz@outlook.com>
2026-03-05 14:28:13 +0800

    [chore]: format

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,84 +1,88 @@
-return {
-    {
-        "stevearc/conform.nvim",
-        -- event = "BufWritePre", -- uncomment for format on save
-        opts = require "configs.conform",
-    },
-
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            require "configs.lspconfig"
-        end,
-    },
-    {
-        "hrsh7th/cmp-cmdline",
-        event = {"CmdlineEnter", "InsertEnter"},
-        dependencies = {
-            "hrsh7th/nvim-cmp",
-            "hrsh7th/cmp-buffer",
+if vim.g.vscode then
+    return {}
+else
+    return {
+        {
+            "stevearc/conform.nvim",
+            -- event = "BufWritePre", -- uncomment for format on save
+            opts = require "configs.conform",
         },
-        config = function()
-            local cmp = require("cmp")
-            cmp.setup({
-                preselect = cmp.PreselectMode.None,
-                completion = {
-                    completeopt = 'menu,menuone,noselect',
-                },
-                mapping = {
-                    ["<CR>"] = cmp.mapping(function(fallback)
-                        if cmp.visible() and cmp.get_selected_entry() then
-                            cmp.confirm({ select = false })
-                        else
-                            fallback()
-                        end
-                    end, { "i", "s" }),
-                    ["<Tab>"] = cmp.mapping.select_next_item(),
-                    ["<S-Tab>"] = cmp.mapping.select_prev_item(),
-                },
-            })
-            cmp.setup.cmdline({ "/", "?" }, {
-                mapping = cmp.mapping.preset.cmdline(),
-                sources = {
-                    { name = "buffer" },
-                },
-            })
-            cmp.setup.cmdline(":", {
-                mapping = cmp.mapping.preset.cmdline(),
-                sources = cmp.config.sources({
-                    { name = "path" },
-                }, {
-                    { name = "cmdline" },
-                }),
-            })
-        end
-    },
-    {
-        "wakatime/vim-wakatime",
-        lazy = false,
-    },
-    {
-        "Mofiqul/dracula.nvim",
-    },
-    {
-        "lukas-reineke/indent-blankline.nvim",
-        main = "ibl",
-        opts = {
-            scope = {
-                enabled = true,
-                show_start = true,
-                show_end = true,
-                char = "┋",
+    
+        {
+            "neovim/nvim-lspconfig",
+            config = function()
+                require "configs.lspconfig"
+            end,
+        },
+        {
+            "hrsh7th/cmp-cmdline",
+            event = {"CmdlineEnter", "InsertEnter"},
+            dependencies = {
+                "hrsh7th/nvim-cmp",
+                "hrsh7th/cmp-buffer",
             },
-        }
-    },
-    {
-        "nvim-tree/nvim-tree.lua",
-        opts = {
-            filters = {
-                dotfiles = false,
-                git_ignored = false
+            config = function()
+                local cmp = require("cmp")
+                cmp.setup({
+                    preselect = cmp.PreselectMode.None,
+                    completion = {
+                        completeopt = 'menu,menuone,noselect',
+                    },
+                    mapping = {
+                        ["<CR>"] = cmp.mapping(function(fallback)
+                            if cmp.visible() and cmp.get_selected_entry() then
+                                cmp.confirm({ select = false })
+                            else
+                                fallback()
+                            end
+                        end, { "i", "s" }),
+                        ["<Tab>"] = cmp.mapping.select_next_item(),
+                        ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+                    },
+                })
+                cmp.setup.cmdline({ "/", "?" }, {
+                    mapping = cmp.mapping.preset.cmdline(),
+                    sources = {
+                        { name = "buffer" },
+                    },
+                })
+                cmp.setup.cmdline(":", {
+                    mapping = cmp.mapping.preset.cmdline(),
+                    sources = cmp.config.sources({
+                        { name = "path" },
+                    }, {
+                        { name = "cmdline" },
+                    }),
+                })
+            end
+        },
+        {
+            "wakatime/vim-wakatime",
+            lazy = false,
+        },
+        {
+            "Mofiqul/dracula.nvim",
+        },
+        {
+            "lukas-reineke/indent-blankline.nvim",
+            main = "ibl",
+            opts = {
+                scope = {
+                    enabled = true,
+                    show_start = true,
+                    show_end = true,
+                    char = "┋",
+                },
+            }
+        },
+        {
+            "nvim-tree/nvim-tree.lua",
+            opts = {
+                filters = {
+                    dotfiles = false,
+                    git_ignored = false
+                }
             }
         }
     }
-}
+end

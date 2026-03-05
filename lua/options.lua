89f52ea0545bdb81f8b648fcff3d655d20a16a08
ahaooahaz <ahaooahaz@outlook.com>
2026-03-05 11:18:09 +0800

    [feat]: ssh share system clipboard

--- lua/options.lua
+++ lua/options.lua
@@ -7,3 +7,16 @@ require "nvchad.options"
 -- local o = vim.o
 -- o.cursorlineopt = "both" -- to enable cursorline!
 end
+if vim.env.SSH_CONNECTION then
+  vim.g.clipboard = {
+    name = "osc52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+      ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+    },
+  }
+end

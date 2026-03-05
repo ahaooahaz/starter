0716fecc7fe47a71e192e1d2bc90dcecd10ddc29
ahaooahaz <ahaooahaz@outlook.com>
2026-03-05 13:51:12 +0800

    [bindkey]: <x|X> do not copy to clipboard

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -14,3 +14,6 @@ map("i", "jk", "<ESC>")
 map({ "n", "v" }, "d", '"_d', { noremap = true, silent = true })
 map({ "n", "v" }, "D", '"_D', { noremap = true, silent = true })
 map("n", "dd", '"_dd', { noremap = true, silent = true })
+
+map({ "n", "v" }, "x", '"_x', { noremap = true, silent = true })
+map({ "n", "v" }, "X", '"_X', { noremap = true, silent = true })
